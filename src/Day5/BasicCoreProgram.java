0d216301fe1ac80cf45e4a6815a0e44697671670
murtazamalik <murtazamalik1319@gmail.com>
2023-02-28 19:23:43 +0530

    Adding Swap Two Number

--- src/Day5/BasicCoreProgram.java
+++ src/Day5/BasicCoreProgram.java
@@ -5,15 +5,20 @@ import java.util.Scanner;
 public class BasicCoreProgram {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int divisor, divide;
-        System.out.println("Enter Quotients Number");
-        divide = scanner.nextInt();
-        System.out.println("Enter Remainder Number");
-        divisor = scanner.nextInt();
-        int quotient = divide / divisor;
-        int remainder = divide % divisor;
-        System.out.println("Quotient : " + quotient);
-        System.out.println("Remainder :" + remainder);
+        int a, b;
+        System.out.println("Enter the number of a");
+        a = scanner.nextInt();
+        System.out.println("Enter the Number of b");
+        b = scanner.nextInt();
+        System.out.println("Before Swap :" + a + " " + b);
+
+        int temp;
+        temp = a;
+        a = b;
+        b = temp;
+
+        System.out.println("After Swap :" + a +" " + b);
+
     }
 
 }
