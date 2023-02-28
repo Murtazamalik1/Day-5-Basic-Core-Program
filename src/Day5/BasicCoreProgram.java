1364651cf717b2ee50efb43f3a46784f8e38d4fb
murtazamalik <murtazamalik1319@gmail.com>
2023-02-28 19:48:37 +0530

    Adding Vowel Or Consonant

--- src/Day5/BasicCoreProgram.java
+++ src/Day5/BasicCoreProgram.java
@@ -5,17 +5,15 @@ import java.util.Scanner;
 public class BasicCoreProgram {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int num;
-        System.out.println("Enter any number To Check Whether Number Is Even Or odd");
-        num = scanner.nextInt();
-
-        if (num % 2 == 0){
-            System.out.println("Number is Even :" + num);
+        char alphabet;
+        System.out.println("Enter any Alphabet");
+        alphabet = scanner.next().charAt(0);
+        if (alphabet == 'a' || alphabet == 'e' || alphabet == 'i' || alphabet == 'o' || alphabet == 'u'){
+            System.out.println("Alphabet is Vowel :" + alphabet);
         }
-        else {
-            System.out.println("Number Is Odd :" + num);
+        else{
+            System.out.println("Alphabet Is Consonant :" + alphabet);
         }
-
     }
 
 }
