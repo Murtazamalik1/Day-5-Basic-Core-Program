df295a46173c28dea14c8caa88a2635c96dadbac
murtazamalik <murtazamalik1319@gmail.com>
2023-02-28 19:15:16 +0530

    Adding Quotient And Remainder

--- src/Day5/BasicCoreProgram.java
+++ src/Day5/BasicCoreProgram.java
@@ -4,19 +4,16 @@ import java.util.Scanner;
 
 public class BasicCoreProgram {
     public static void main(String[] args) {
-        int N;
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter Any Number");
-        N = scanner.nextInt();
-        for (int i = 2; i * i <= N; i++){
-            if (N%i == 0){
-                N = N/i;
-                System.out.println(i+ " ");
-            }
-        }
-        if (N != 0){
-            System.out.println(N);
-        }
+        int divisor, divide;
+        System.out.println("Enter Quotients Number");
+        divide = scanner.nextInt();
+        System.out.println("Enter Remainder Number");
+        divisor = scanner.nextInt();
+        int quotient = divide / divisor;
+        int remainder = divide % divisor;
+        System.out.println("Quotient : " + quotient);
+        System.out.println("Remainder :" + remainder);
     }
 
 }
