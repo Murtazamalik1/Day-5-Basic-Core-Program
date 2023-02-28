3daa0fdeb7fb4a101fdec4dfc819e575dab479a4
murtazamalik <murtazamalik1319@gmail.com>
2023-02-28 20:04:59 +0530

    Adding Find Largest Among Three Number

--- src/Day5/BasicCoreProgram.java
+++ src/Day5/BasicCoreProgram.java
@@ -5,15 +5,23 @@ import java.util.Scanner;
 public class BasicCoreProgram {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        char alphabet;
-        System.out.println("Enter any Alphabet");
-        alphabet = scanner.next().charAt(0);
-        if (alphabet == 'a' || alphabet == 'e' || alphabet == 'i' || alphabet == 'o' || alphabet == 'u'){
-            System.out.println("Alphabet is Vowel :" + alphabet);
+        int num1,num2,num3;
+        int large;
+        System.out.println("Enter First Number :");
+        num1 = scanner.nextInt();
+        System.out.println("Enter Second Number :");
+        num2 = scanner.nextInt();
+        System.out.println("Enter Third Number :");
+        num3 = scanner.nextInt();
+
+        if (num1 > num2 || num1 > num3){
+            large = num1;
+        } else if (num2 > num3) {
+            large = num2;
         }
         else{
-            System.out.println("Alphabet Is Consonant :" + alphabet);
+            large = num3;
         }
+        System.out.println(" The Largest Number Among Three Is :" +large);
     }
-
 }
